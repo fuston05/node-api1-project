960607910a658e9c98582e188dad97601d508ac3
fuston05 <fuston05@yahoo.com>
2020-03-09 16:45:02 -0400

    got the getById endpoint to work

--- index.js
+++ index.js
@@ -3,7 +3,44 @@ const shortid= require('shortid');
 
 const server= express().use(express.json());
 
-server.post('/api/users');
+//data
+const users= [];
+
+server.post('/api/users', (req, res) => {
+  if(req.body.name && req.body.bio){
+    const user= req.body;
+    user.id= shortid.generate();
+    if( users.push(user) ){
+      res.status(201).json(user);
+    }else{
+      error= {"error": "There was an error while savingk the user to the database"}
+      res.status(500).json(error)
+    }//end if push user
+    
+  }else{
+    const error= {"errorMessage": "please provide a name and bio for the user"}
+    res.status(400).json(error);
+  }//end if
+  
+});
+
+server.get('/api/users', (req, res) => {
+  if( res.status(200).json(users) ){
+    res.status(200).json(users);
+  }else{
+    error= {"error": "The users information could not be retrieved"}
+    res.status(500).json(error)
+  }
+});
+
+server.get('/api/users/:id', (req, res) => {
+  const userById= users.filter(user => {
+    return user.id === req.params.id
+  });
+  res.status(200).json(userById);
+});
+
+
 
 
 const PORT= 5000;
