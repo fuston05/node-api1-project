f1012cc14e209860c9f19c6353d844b02859c769
fuston05 <fuston05@yahoo.com>
2020-03-09 16:52:18 -0400

    got all getUsers, getUsersById, and post enpoints done with all error messages.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ server.post('/api/users', (req, res) => {
     if( users.push(user) ){
       res.status(201).json(user);
     }else{
-      error= {"error": "There was an error while savingk the user to the database"}
+      error= {"error": "There was an error while saving the user to the database"}
       res.status(500).json(error)
     }//end if push user
     
@@ -25,7 +25,7 @@ server.post('/api/users', (req, res) => {
 });
 
 server.get('/api/users', (req, res) => {
-  if( res.status(200).json(users) ){
+  if(users.length > 0){
     res.status(200).json(users);
   }else{
     error= {"error": "The users information could not be retrieved"}
@@ -37,7 +37,12 @@ server.get('/api/users/:id', (req, res) => {
   const userById= users.filter(user => {
     return user.id === req.params.id
   });
-  res.status(200).json(userById);
+  if(userById === []){
+    res.status(200).json(userById);
+  }else{
+    error= {"error": "The user with the specified ID does not exist"}
+    res.status(404).json(error);
+  }
 });
 
 
